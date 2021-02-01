b4e096150adcc4c2ab5ba8c2158a140cbc932b6c
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2021-02-02 03:18:26 +0600

    lab-2 problem01 in process

---
+++ lab-02/src/Canvas.java
@@ -0,0 +1,29 @@
+public class Canvas {
+    public static final int SIZE = 20;
+    private char[][] data;
+
+    public Canvas(){
+        data = new char[SIZE][SIZE];
+        for(int row = 0; row< SIZE; row++){
+            for(int col = 0; col< SIZE; col++){
+                data[row][col]='.';
+            }
+        }
+    }
+
+    public void set(int row, int col) {
+        if(row<0 || SIZE<=row || col<0 || SIZE<=col){
+            throw new RuntimeException("Canvas: coordinates are out of Canvas");
+        }
+        data[row][col]='*';
+    }
+
+    public void show() {
+        for(int i=0; i<SIZE; i++){
+            for(int j=0; j<SIZE; j++){
+                System.out.print(data[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ lab-02/src/Problem01.java
@@ -0,0 +1,8 @@
+public class Problem01 {
+    public static void main(String[] args) {
+
+        Canvas canvas = new Canvas();
+        canvas.set(10,10);
+        canvas.show();
+    }
+}
