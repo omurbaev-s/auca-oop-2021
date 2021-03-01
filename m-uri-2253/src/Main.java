67b95a45c1d1c418774516ad7d460f64926998a3
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2021-03-02 04:18:48 +0600

    m-uri-2253 accepted

--- m-uri-2253/src/Main.java
+++ m-uri-2253/src/Main.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        int counter=0;
-        int counter1=0;
-        int counter2=0;
-        int counter3=0;
+        int counter;
+        int counter1;
+        int counter2;
+        int counter3;
 
         while (inp.hasNextLine()) {
             counter = 0;
             counter1 = 0;
             counter2 = 0;
             counter3 = 0;
+
             String str = inp.nextLine();
             char[] array = str.toCharArray();
             if (array.length >= 6 && array.length <= 32){
@@ -20,17 +21,21 @@ public class Main {
                     if (Character.isDigit(c)) {
                         counter1++;
                     }
-                    if (!Character.isUpperCase(c)) {
+                    if (Character.isUpperCase(c)) {
                         counter++;
+                    } else if(!Character.isDigit(c) && !Character.isLowerCase(c)) {
+                        counter3++;
                     }
-                    if (!Character.isLowerCase(c)) {
+                    if (Character.isLowerCase(c)) {
                         counter2++;
+                    } else if(!Character.isDigit(c) && !Character.isUpperCase(c)) {
+                        counter3++;
                     }
                     if (c == ' ') {
                         counter3++;
                     }
                 }
-        }
+            }
             if(counter!=0 && counter1!=0 && counter2!=0 && counter3==0){
                 System.out.println("Senha valida.");
             } else{
