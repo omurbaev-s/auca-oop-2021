f0e99ff431a5a946abe4af18d4f1308f52253d1c
Sanzhar Omurbaev <omurbaev_s@auca.kg>
2021-03-02 08:23:13 +0600

    lab-3 lecture07

--- lab-03/src/Problem01.java
+++ lab-03/src/Problem01.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Problem01 {
+    Scanner inp = new Scanner(System.in);
     public static void main(String[] args) {
-        Rational a = new Rational(1, 2);
-        Rational b = new Rational(2, 3);
+        Rational a = new Rational(1, -30);
+        Rational b = new Rational(1, -10);
 
         System.out.printf("%s + %s = %s%n", a, b, a.add(b));
+        System.out.printf("%s - %s = %s%n", a, b, a.subtract(b));
+        System.out.printf("%s * %s = %s%n", a, b, a.multiply(b));
+        System.out.printf("%s / %s = %s%n", a, b, a.divide(b));
+        System.out.printf("%s.compareTo(%s) = %s%n", a, b, a.compareTo(b));
 
 
     }
--- lab-03/src/Rational.java
+++ lab-03/src/Rational.java
@@ -6,8 +6,22 @@ public class Rational {
         if(den == 0){
             throw new RuntimeException("Rational: denominator is equal to zero");
         }
-        this.num = num;
-        this.den = den;
+        if(den<0){
+            num = -num;
+            den = -den;
+        }
+
+        int a = Math.abs(num);
+        int b = Math.abs(den);
+
+        while(b!=0){
+            int rem = a % b;
+            a = b;
+            b = rem;
+        }
+
+        this.num = num/a;
+        this.den = den/a;
     }
     @Override
     public String toString(){
@@ -18,5 +32,37 @@ public class Rational {
         int rNum = num * other.den + den*other.num;
         int rDen = den * other.den;
         return new Rational(rNum, rDen);
+    }
+
+    public Rational subtract(Rational other) {
+        int rNum = num * other.den - den*other.num;
+        int rDen = den * other.den;
+        return new Rational(rNum, rDen);
+    }
+
+    public Rational multiply(Rational other) {
+        int rNum = num * other.num;
+        int rDen = den * other.den;
+        return new Rational(rNum, rDen);
+    }
+
+    public Rational divide(Rational other) {
+        if(other.num==0){
+            throw new RuntimeException("Rational: division by zero");
+        }
+        int rNum = num * other.den ;
+        int rDen = den * other.num;
+        return new Rational(rNum, rDen);
+    }
+
+    public int compareTo(Rational other) {
+        int lhs = num * other.den;
+        int rhs = other.num * den;
+        if(lhs<rhs){
+            return -1;
+        } else if(lhs>rhs){
+            return 1;
+        }
+        return 0;
     }
 }
